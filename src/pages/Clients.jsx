2b4ad522016cb715fe206861b3b3a5813fac668d
Abdoul Ibrahima Samake <samabdoul03@gmail.com>
2024-04-15 18:53:54 +0000

    pages stylised

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,12 +1,12 @@
-import google from "../assets/Google play.png";
-import Apple from "../assets/AppStore.svg";
+// import google from "../assets/Google play.png";
+// import Apple from "../assets/AppStore.svg";
 import "../style/client.css";
 import online from "../assets/online_groceries.svg";
 
 const Clients = () => {
   return (
     <div className="contener_flex flex flex-row justify-center mt-70 ml-20">
-      <div className="left w-2/4 text-center font-bold">
+      <div className="left w-2/4 text-center font-bold ml-5">
         <h2 className="t2"> Comment ca Marche ?</h2>
         <p>
           C&apos;est simple telecharger notre Application Gratuitement sur
@@ -16,10 +16,10 @@ const Clients = () => {
           votre commande.
         </p>
       </div>
-      <div className="rigth  w-2/4 mb-0">
-        <img src={online} alt="App" />
-        <img src={Apple} alt="AppStore" />
-        <img src={google} alt="playStore" width="140px" />
+      <div className="rigth w-2/4 pt-10 ">
+        <img src={online} alt="App" className="app" />
+        {/* <img src={Apple} alt="AppStore" />
+        <img src={google} alt="playStore" width="140px" /> */}
       </div>
     </div>
   );
