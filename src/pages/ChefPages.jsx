e5c9fae3a841e479713c305a4ddef9f355d6063f
Abdoul Ibrahima Samake <samabdoul03@gmail.com>
2024-05-05 18:53:27 +0000

    back okay

--- src/pages/ChefPages.jsx
+++ src/pages/ChefPages.jsx
@@ -1,54 +1,10 @@
 /* eslint-disable react/prop-types */
-// import axios from "axios";
-// import { useEffect, useState } from "react";
 
 const ChefPages = ({ chefs }) => {
-  // const [nom, setNom] = useState("");
-  // const [prenom, setPrenom] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [adresse, setAdresse] = useState("");
-  // const [telephone, setTelephone] = useState("");
-  // const [profil, setProfil] = useState("");
-  // const [chefs, setChefs] = useState([]);
-  // const { id } = useParams();
-  // useEffect(() => {
-  //   axios
-  //     .get(`http://localhost:5000/chef/`)
-  //     .then((res) => {
-  //       setChefs(res.data);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
-  // }, []);
+  console.log(chefs);
   return (
     <>
-      {chefs.map((chef) => (
-        <div
-          className="flex flex-col border-2 border-white rounded-xl"
-          key={chef.id}
-        >
-          <h1>Bienvenue {chef.prenom}</h1>
-          <div className="profil">{chef.profil}</div>
-          <div className="flex flex-row my-4 ">
-            <span className="text-center text-gray-700 text-2xl">
-              {chef.prenom}
-            </span>
-            <span className="text-center text-gray-700 text-2xl">
-              {chef.nom}
-            </span>
-            <span className="text-center text-gray-700 text-2xl">
-              {chef.email}
-            </span>
-            <span className="text-center text-gray-700 text-2xl">
-              {chef.adresse}
-            </span>
-            <span className="text-center text-gray-700 text-2xl">
-              {chef.telephone}
-            </span>
-          </div>
-        </div>
-      ))}
+      <h1>Bonjour</h1>
     </>
   );
 };
--- src/pages/Connexion.jsx
+++ src/pages/Connexion.jsx
@@ -2,7 +2,6 @@ import { NavLink, useNavigate } from "react-router-dom";
 import "../style/conn.css";
 import { useState } from "react";
 import axios from "axios";
-// import * as jwt from "jwt-decode";
 
 const Connexion = () => {
   const [telephone, setTelephone] = useState("");
@@ -20,16 +19,8 @@ const Connexion = () => {
       .post("http://localhost:5000/chef/login", data)
       .then((res) => {
         alert("Connexion réussie");
-        ////////without Token
-        console.log(res.data.id);
-        // const token = res.data.token;
-        // //// Décoder le token JWT pour obtenir les données utilisateur
-        // const decodedToken = jwt(token);
-        // console.log(decodedToken);
-        // // console.log(token);
-        // //// Stocker les données utilisateur dans l'état du chef
-        // setChefs(decodedToken);
-        // navigate("/ChefPages", { chefs });
+        setChefs(res.data);
+        navigate("/ChefPages", { chefs });
       })
       .catch((error) => {
         alert("Erreur de Connection");
@@ -63,7 +54,7 @@ const Connexion = () => {
           />
           <input type="submit" value="Se connecter" />
           <p>
-            Vous n&apos;savez pas un compte ?{" "}
+            Vous n&apos;savez pas de compte ?{" "}
             <NavLink to="/inscription">Inscrivez-vous</NavLink>
           </p>
         </form>
