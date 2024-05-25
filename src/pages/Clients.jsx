c79cca294746369128133fadb77920b7b27482bd
Abdoul Ibrahima Samake <samabdoul03@gmail.com>
2024-05-25 06:00:26 +0000

    2changing design

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,3 +1,7 @@
+import localisation from "../assets/localisation.jpg";
+import commande from "../assets/food3.jpg";
+import livre from "../assets/livre.jpg";
+
 const Clients = () => {
   return (
     <div className="flex flex-col justify-center mt-70 bg-white">
@@ -11,29 +15,31 @@ const Clients = () => {
         </p>
       </div>
       <div className="flex flex-row justify-between pt-20">
-        <div>
-          <div></div>
-          <p className="font-semibold text-2xl">Indiquez nous ou vous etes</p>
-          <span>
+        <div className="ml-5">
+          <div>
+            <img src={localisation} alt="" className="w-32 h-32" />
+          </div>
+          <p className="font-bold text-xl">Indiquez nous ou vous etes</p>
+          <span className="text-xs">
             Nous vous indiquerons les partenaires a proximite chez qui vous
             pouvez commander
           </span>
         </div>
         <div>
-          <div></div>
-          <p className="font-semibold text-2xl">
-            Trouvez ce dont vous avez envie
-          </p>
-          <span>
+          <div>
+            <img src={commande} alt="" className="w-32 h-36" />
+          </div>
+          <p className="font-bold text-xl">Trouvez ce dont vous avez envie</p>
+          <span className="text-xs">
             Cherchez par article ou repas, par partenaire ou type de cuisinie.
           </span>
         </div>
         <div>
-          <div></div>
-          <p className="font-semibold text-2xl">
-            Commandez a livrer ou a emporter
-          </p>
-          <span>
+          <div>
+            <img src={livre} alt="" className="w-32 h-32" />
+          </div>
+          <p className="font-bold text-xl">Commandez a livrer ou a emporter</p>
+          <span className="text-xs">
             Nous vous tiendrons au courant du deroulement de votre commande.
           </span>
         </div>
---
+++ src/pages/Telecharger.js
@@ -0,0 +1,9 @@
+function Telecharger() {
+  return (
+    <div className="flex flex-row justify-between">
+      <p>Telecharger l&apos;application</p>
+    </div>
+  );
+}
+
+export default Telecharger;
